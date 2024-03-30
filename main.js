b1946b64c49e3ef6f43fe29e81bd83415aedbd2c
SkyIsLostAgain <136880731+SkyIsLostAgain@users.noreply.github.com>
2024-03-30 14:03:07 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ function preload(){
 function setup(){
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
 
 function draw(){
